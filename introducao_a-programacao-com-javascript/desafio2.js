d6e732cb8f915b583ae51382ece5a642444ee67d
Cassio Oliveira <cassio@cassioliveira.com.br>
2021-05-24 10:25:50 -0300

    Challenges 1

---
+++ introducao_a-programacao-com-javascript/desafio2.js
@@ -0,0 +1,4 @@
+let value1 = parseInt(gets());
+let value2 = parseInt(gets());
+let total = value1 * value2;
+console.log("PROD = " + total);
---
+++ introducao_a-programacao-com-javascript/desafio3.js
@@ -0,0 +1,6 @@
+let value1 = parseInt(gets());
+let value2 = parseInt(gets());
+let value3 = parseFloat(gets());
+let salary = parseFloat(value2 * value3).toFixed(2); // Digite aqui o calculo do salário
+console.log(`NUMBER = ${value1}`);
+console.log(`SALARY = U$ ${salary}`);
